820f1db8cdec19e8a98adcf1915c7177060cc062
forandrea <156220661+forandrea@users.noreply.github.com>
2025-03-27 11:39:38 -0400

    Add project files.

---
+++ InClassAssignment_03272025/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassAssignment_03272025
+{
+        internal class SayHello
+        {
+            /// <summary>
+            /// Write a friendly message to the console
+            /// </summary>
+            public void Hello()
+            {
+                Console.WriteLine("Hello from the SayHello class");
+            }
+
+        }
+ }
+
